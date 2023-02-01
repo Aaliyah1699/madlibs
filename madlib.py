3f1f674c8a3ce0f0394600e3e53de1d3bca76ea6
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-01 23:16:14 +0000

    replaced noun with thing and created input for it

--- madlib.py
+++ madlib.py
@@ -5,9 +5,12 @@
 '''Get user word input'''
 
 name = input("Enter a name: ")
-noun = input("Enter a noun: ")
+thing = input("Enter a thing: ")
 verb = input("Enter a verb: ")
 song = input("Enter a song: ")
 artist = input("Enter the artist: ")
 
 '''Print out story including user input'''
+
+print("My name is", name,"and I love",thing".")
+print("I also collect photos of ")
