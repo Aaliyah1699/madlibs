a2148a8cb8da97f2c1f45967b255d7a953bf8426
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-01 23:13:13 +0000

    Added input options for users

--- madlib.py
+++ madlib.py
@@ -4,4 +4,10 @@
 
 '''Get user word input'''
 
+name = input("Enter a name: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+song = input("Enter a song: ")
+artist = input("Enter the artist: ")
+
 '''Print out story including user input'''
