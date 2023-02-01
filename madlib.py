08f6ac1114115772e0f08554f207aebcff7e6bce
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-01 23:25:48 +0000

    removed word so sentence makes more sense

--- madlib.py
+++ madlib.py
@@ -14,5 +14,5 @@ artist = input("Enter the artist: ")
 
 print("My name is", name,"and I love",thing)
 print("I also collect photos of", thing,"anytime I find a new one.")
-print("I also started", verb,"daily, and enjoy it quite a bit.")
+print("I also", verb,"daily, and enjoy it quite a bit.")
 print("My favorite song is", song,"by",artist,"I listen to it while I",verb)
