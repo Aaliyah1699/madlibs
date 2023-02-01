72fe581b921e77f0cc2547c0c84ab4eff0137ed1
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-02-01 23:26:45 +0000

    finished and saved file

--- madlib.py
+++ madlib.py
@@ -12,7 +12,8 @@ artist = input("Enter the artist: ")
 
 '''Print out story including user input'''
 
-print("My name is", name,"and I love",thing)
-print("I also collect photos of", thing,"anytime I find a new one.")
-print("I also", verb,"daily, and enjoy it quite a bit.")
-print("My favorite song is", song,"by",artist,"I listen to it while I",verb)
+print("My name is", name, "and I love", thing)
+print("I also collect photos of", thing, "anytime I find a new one.")
+print("I also", verb, "daily, and enjoy it quite a bit.")
+print("My favorite song is", song, "by",
+      artist, "I listen to it while I", verb)
